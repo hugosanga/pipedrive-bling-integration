296c6b5556b7bdf212f9d0e3810e7a51f2ddf211
Hugo Sangaletti <hugo.fsangaletti@gmail.com>
2021-05-11 23:50:59 -0300

    [FIX] Unused import

--- src/deals/deals.module.ts
+++ src/deals/deals.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ThrottlerModule } from '@nestjs/throttler';
 import { DealsController } from './deals.controller';
 import { Deals, DealsSchema } from './deals.schema';
 import { DealsService } from './deals.service';
